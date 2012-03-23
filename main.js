4f8cf684fbd45751de91c4e32b276ca2c4c51154
Valerio Proietti <kamicane@gmail.com>
2012-03-23 19:02:28 +0100

    using private key now instead of constructed methods, which is faster (ty sebmarkbage)

--- main.js
+++ main.js
@@ -7,62 +7,70 @@ var prime = require("prime/prime"),
 
 var instances = {}
 
-var Node = function(node){
+var key = "n" + Math.round((Math.random() * 1e5)).toString(36)
 
-    this.node = function(i){
-        return i ? null : node
-    }
+var Node = prime({
 
-    this.nodes = function(s, e){
-        return [node].slice(s, e)
-    }
+    constructor: function Node(node){
+        this[key] = node
+    },
 
-    this.count = function(){
-        return 1
-    }
+    node: function(i){
+        return i ? null : this[key]
+    },
 
-    this.handle = function(ƒ){
-        var buffer = []
-        var res = ƒ.call(this, node, 0, buffer)
+    nodes: function(begin, end){
+        return [this[key]].slice(begin, end)
+    },
+
+    handle: function(method){
+        var buffer = [], node = this[key]
+        var res = method.call(this, node, 0, buffer)
         if (res != null && res !== false && res !== true) buffer.push(res)
         return buffer
     }
 
-}
+})
 
 // Nodes
 
-var Nodes = function(nodes){
+var Nodes = prime({
 
-    this.node = function(i){
-        var node = nodes[i == null ? 0 : i]
+    inherits: Node,
+
+    constructor: function Nodes(nodes){
+        this[key] = nodes
+    },
+
+    node: function(i){
+        var node = this[key][i == null ? 0 : i]
         return node ? node : null
-    }
+    },
 
-    this.nodes = function(s, e){
-        return array.slice(nodes, s, e)
-    }
+    nodes: function(begin, end){
+        return array.slice(this[key], begin, end)
+    },
 
-    this.count = function(){
-        return nodes.length
-    }
+    count: function(){
+        return this[key].length
+    },
 
-    this.handle = function(ƒ){
-        var buffer = []
+    handle: function(method){
+        var buffer = [], nodes = this[key]
         for (var i = 0, l = nodes.length; i < l; i++){
             var node = nodes[i],
-                res = ƒ.call(new Node(node), node, i, buffer)
+                res = method.call(new Node(node), node, i, buffer)
             if (res === false || res === true) break
             if (res != null) buffer.push(res)
         }
         return buffer
     }
 
-}
+})
 
 var $ = prime({constructor: function(nodes){
     if (nodes == null) return null
-    if (nodes instanceof Nodes || nodes instanceof Node) return nodes
+    if (nodes instanceof Node) return nodes
 
     if (typeof nodes === "string"){
         nodes = $.querySelectorAll(document, nodes)
@@ -77,7 +85,7 @@ var $ = prime({constructor: function(nodes){
     return new Nodes(nodes)
 }})
 
-Nodes.prototype = Node.prototype = $.prototype
+$.prototype = Node.prototype
 
 $.querySelectorAll = function(context, selector){
     return context.querySelectorAll ? context.querySelectorAll(selector) : null
