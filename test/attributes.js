5cd840b649e922411eeb0af25a01997d075b3cee
Arian <arian@aryweb.nl>
2012-05-07 22:20:34 +0200

    Basic test server for testing in the browser

---
+++ test/attributes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var $ = require('../lib/nodes')
+var expect = require('expect.js')
+
+describe('setAttribute', function(){
+
+})
+
+describe('getAttribute', function(){
+
+})
+
+describe('hasAttribute', function(){
+
+})
+
+describe('removeAttribute', function(){
+
+})
+
